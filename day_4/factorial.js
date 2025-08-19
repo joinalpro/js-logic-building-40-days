469e1dd0d9f8046abc86fe9e61c46ba64bfda4d8
Joinal Abden <joinalzx@gmail.com>
2025-08-19 19:38:46 +0600

    day 4 - factorial number, recursion

--- day_4/factorial.js
+++ day_4/factorial.js
@@ -2,7 +2,7 @@
 // The factorial of a number is a mathematic concept used in many areas like counting possibilities and solving problems. The factorial of a number n, written as n!, is the result of multiplying all the whole numbers from 1 up to n.
 
 /*
-n! = n * (n-1)* n(n-2)*
+n! = n * (n-1)*(n-2)*
 */
 
 // function factNumb(fact) {
@@ -33,5 +33,6 @@ function factorialNum(num) {
 
 console.log(factorialNum(4));
 console.log(factorialNum(6));
+console.log(factorialNum(10));
 console.log(factorialNum(0));
 console.log(factorialNum(1));
